fe8bd9214c746de4db59da6026339365a79d4513
Deok <dead.pool.aka.wilson@gmail.com>
2024-04-06 17:16:03 +0900

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex max-w-[430px] min-h-screen flex-col items-center justify-between p-24"></main>
+  );
+}
