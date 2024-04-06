735f370d7ce39aded5a6d4a1721f91bbd9b17a22
Deok <dead.pool.aka.wilson@gmail.com>
2024-04-06 17:23:29 +0900

    set page directory

--- src/app/(assigned)/home/page.tsx
+++ src/app/(assigned)/home/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-
-export default function main() {
+export default function Home() {
   return (
     <main className="flex max-w-[430px] min-h-screen flex-col items-center justify-between p-24"></main>
   );
