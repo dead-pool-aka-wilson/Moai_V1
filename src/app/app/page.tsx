48760a9ce4e599f2bb92c564bd5cddcea62cd00e
Deok <dead.pool.aka.wilson@gmail.com>
2024-04-06 22:07:45 +0900

    add start page view

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Page } from "./_components";
+import { Page } from "@/app/_components";
 
 export default function Home() {
   return (
--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import { Page } from "../_components";
+
 export default function Start() {
   return (
-    <main className="flex min-h-screen max-w-[430px] flex-col items-center justify-between p-24">
-      start
-    </main>
+    <Page>
+      <div className="grow flex flex-col items-center justify-center">
+        <div className="h-[320px] w-[320px] relative">
+          <Image src="/images/moai.png" fill alt="MOAI" />
+        </div>
+        <div className="flex mt-[20px] gap-5">
+          <p className="text-4xl font-bold text-center text-purple-600 ">
+            MOAI
+          </p>
+          <p className="text-lg">meme of all internet</p>
+        </div>
+      </div>
+      <div className="w-full flex-col justify-between items-center uppercase">
+        <div className="px-[20px] py-[14px] bg-gray-300 w-full text-center font-bold rounded-full mb-[20px]">
+          import secret key
+        </div>
+        <div className="px-[20px] py-[14px] bg-purple-600 text-white w-full text-center font-bold rounded-full">
+          create new key
+        </div>
+      </div>
+    </Page>
   );
 }
